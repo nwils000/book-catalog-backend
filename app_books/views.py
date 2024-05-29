148a123cec64ffdedbb68736f7b138fd00b5b58a
nwils000 <nwils000@gmail.com>
2024-05-29 10:35:47 -0400

    added basic profile model and get method from /profile/ endpoint

--- app_books/views.py
+++ app_books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_profile(request):
+    serialized_profile = ProfileSerializer(request.user.profile)
+    return Response(serialized_profile.data)
